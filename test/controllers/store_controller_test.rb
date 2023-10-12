f1d56bcf88c9a3862367093392c02bc690aedbfc
William Martin <maartz@icloud.com>
2023-10-12 15:02:38 +0200

    add store controller and update layout

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class StoreControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get store_index_url
+    assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 1
+    assert_select 'h2', 'Programming Elixir'
+  end
+end
